6b21b6440b1fd1f44fd5d3c295eba432f33ea154
Ayishumol <100148504+Ayishumol@users.noreply.github.com>
2022-02-22 08:07:25 +0530

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -9,9 +9,9 @@ Ktb.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (a
 //coded by saidali
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:FASIL [OWNER]\n' // full name
-            + 'ORG:FASIL AF;\n' // 
+            + 'FN:ꜰᴀꜱɪʟ [OWNER]\n' // full name
+            + 'ORG:ꜰᴀꜱɪʟ;\n' // 
             + 'TEL;type=CELL;type=VOICE;waid=919947430792:+919947430792\n' // WhatsApp ID + phone number
             + 'END:VCARD'
-await message.client.sendMessage(message.jid,{displayname: "AYISHUMOL [OWNER]", vcard: vcard}, MessageType.contact)
+await message.client.sendMessage(message.jid,{displayname: "ᴀʏɪꜱʜᴜᴍᴏʟ [OWNER]", vcard: vcard}, MessageType.contact)
 }))
